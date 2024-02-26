aee095bb320c7a565524a3aafc7019e804db9db9
Erlend Angelsen <erlend@askeladden.co>
2024-02-26 15:27:51 +0100

    Add MainScreen component and WeatherListItem component

---
+++ app/index.tsx
@@ -0,0 +1,88 @@
+import { WeatherListItem } from "@/components/WeatherListItem";
+import { defaultLocations } from "@/constants/defaultLocations";
+import { Coordinates } from "@/types/globals";
+import * as ExpoLocation from "expo-location";
+import { useEffect, useState } from "react";
+import {
+  Alert,
+  FlatList,
+  SafeAreaView,
+  StatusBar,
+  StyleSheet,
+  Text,
+  View,
+} from "react-native";
+
+export const MainScreen = () => {
+  const [locations, setLocations] = useState<Coordinates[]>(defaultLocations);
+  const renderItem = (item: Coordinates, index: number) => {
+    return <WeatherListItem coords={item} index={index} />;
+  };
+
+  useEffect(() => {
+    (async () => {
+      let { status } = await ExpoLocation.requestForegroundPermissionsAsync();
+      if (status !== "granted") {
+        Alert.alert("Permission to access location was denied");
+        return;
+      }
+
+      let location = await ExpoLocation.getCurrentPositionAsync({});
+      const userLocation = {
+        id: 0,
+        latitude: location.coords.latitude,
+        longitude: location.coords.longitude,
+      };
+      // check if the location is already in the list
+      const exists = locations.find(
+        (l) =>
+          l.latitude === userLocation.latitude &&
+          l.longitude === userLocation.longitude
+      );
+      if (exists) return;
+
+      setLocations((currentLocations) => [userLocation, ...currentLocations]);
+    })();
+  }, []);
+
+  return (
+    <SafeAreaView style={styles.container}>
+      <StatusBar barStyle="dark-content" backgroundColor="#ecf0f1" />
+      <View style={styles.headerContainer}>
+        <Text style={styles.header}>Locations</Text>
+      </View>
+      <FlatList
+        keyExtractor={(i) =>
+          `${i.latitude.toString()}-${i.longitude.toString()}`
+        }
+        data={locations}
+        renderItem={(i) => renderItem(i.item, i.index)}
+      ></FlatList>
+    </SafeAreaView>
+  );
+};
+
+export default MainScreen;
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    padding: 20,
+    backgroundColor: "beige",
+  },
+  headerContainer: {
+    flex: 1,
+    flexDirection: "row",
+    height: 100,
+    maxHeight: 50,
+    paddingHorizontal: 10,
+    justifyContent: "space-between",
+    alignItems: "center",
+  },
+  header: {
+    color: "black",
+    fontSize: 24,
+    marginVertical: 10,
+    fontWeight: "500",
+  },
+});
---
+++ components/WeatherListItem.tsx
@@ -0,0 +1,97 @@
+import { colors } from "@/constants/colors";
+import { SettingsContext } from "@/contexts/SettingsContext";
+import { useReverseGeocoding } from "@/hooks/useReverseGeocoding";
+import { useWeatherForLocation } from "@/hooks/useWeatherForLocation";
+import { Coordinates } from "@/types/globals";
+import { TemperatureUnit, getTemperatureForUnit } from "@/utils/weatherUtils";
+import { useContext } from "react";
+import { Image, StyleSheet, Text, TouchableOpacity, View } from "react-native";
+
+export type WeatherCardProps = {
+  coords: Coordinates;
+  index: number;
+};
+export const WeatherListItem = ({ coords, index }: WeatherCardProps) => {
+  const { isLoading, weatherData, error } = useWeatherForLocation(coords);
+  const address = useReverseGeocoding({
+    latitude: coords.latitude,
+    longitude: coords.longitude,
+  });
+  const settings = useContext(SettingsContext);
+  const unit = settings?.unit ?? TemperatureUnit.Celsius;
+  const color = colors[index % colors.length];
+  const imageUrl = `https://raw.githubusercontent.com/metno/weathericons/main/weather/png/${weatherData?.properties?.timeseries[0]?.data.next_1_hours?.summary.symbol_code}.png`;
+
+  const temperature = getTemperatureForUnit(
+    weatherData?.properties.timeseries[0].data.instant.details
+      .air_temperature ?? 0,
+    unit
+  );
+
+  // show loading or error state
+
+  return (
+    <TouchableOpacity
+      onPress={() => {}}
+      style={[{ backgroundColor: color }, styles.touchableOpacity]}
+    >
+      <View style={styles.viewContent}>
+        <View>
+          <Text style={styles.cityText}>{address.geocodedLocation?.city}</Text>
+          <Text>{address.geocodedLocation?.country}</Text>
+        </View>
+        <View style={styles.temperatureAndImageContainer}>
+          <Text style={{ fontSize: 48 }}>
+            {temperature}
+            {unit}
+          </Text>
+          <Image
+            tintColor={"#000"}
+            style={styles.image}
+            source={{
+              uri: imageUrl,
+            }}
+          />
+        </View>
+      </View>
+    </TouchableOpacity>
+  );
+};
+
+const styles = StyleSheet.create({
+  animatedView: {
+    borderRadius: 10,
+    margin: 10,
+    borderWidth: 2,
+    borderColor: "black",
+  },
+  viewContent: {
+    padding: 10,
+    flex: 1,
+    flexDirection: "row",
+    justifyContent: "space-between",
+  },
+  cityText: {
+    fontWeight: "500",
+    fontSize: 24,
+  },
+  image: {
+    tintColor: "#000",
+    width: 30,
+    height: 30,
+  },
+  temperatureText: {
+    fontSize: 48,
+  },
+  touchableOpacity: {
+    borderRadius: 10,
+    margin: 10,
+    borderWidth: 2,
+    borderColor: "black",
+  },
+  temperatureAndImageContainer: {
+    flex: 1,
+    flexDirection: "row",
+    justifyContent: "flex-end",
+  },
+});
